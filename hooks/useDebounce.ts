a411aa29b27fdb77994c440c56cd340ec851c968
Aswin Karki <aswin.karki44@gmail.com>
2024-04-27 12:04:36 +0545

    feat: added debonce for search

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,2 +1,14 @@
-import { useEffect } from "react";
-const useDebounce = (value: any, delay: number) => {};
+import { useEffect, useState } from "react";
+const useDebounce = (value: any, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
