8b133390101b3310025fe6c49cd862253dfc7ba6
Aswin Karki <aswin.karki44@gmail.com>
2024-04-30 17:18:14 +0545

    bugfix:unecessary view movement when opening keyboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,8 @@ import {
   ScrollView,
   ActivityIndicator,
   Image,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { colors, fontSize } from "../../constants/token";
 import { WeatherData } from "../../types/types";
@@ -56,162 +58,168 @@ const Home = () => {
   let date = d.getDate();
 
   return (
-    <ScrollView
-      contentContainerStyle={
-        weatherDataCtx?.fetching
-          ? {
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: colors.background_cream,
-            }
-          : {
-              backgroundColor: colors.background_cream,
-              flex: 1,
-              justifyContent: "space-between",
-              padding: 6,
-            }
-      }
-    >
-      {weatherDataCtx?.fetching ? (
-        <ActivityIndicator size="large" />
-      ) : (
-        <>
-          {/* locatoin details */}
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              marginLeft: 10,
-              marginTop: 24,
-            }}
-          >
-            <View style={{ flexDirection: "column" }}>
-              <Text
-                style={{ fontFamily: "GlacialIndifference" }}
-              >{`${day}, ${date} ${month}`}</Text>
-              <Text
-                style={{
-                  fontSize: 32,
-                  color: colors.background_dark,
-                  fontFamily: "MaiandraSD",
-                }}
-              >
-                {weatherForecast?.location?.name}
-              </Text>
-              <Text style={{ fontFamily: "GlacialIndifference" }}>
-                {weatherForecast?.location?.country}
-              </Text>
-            </View>
-          </View>
-
-          {/* weather condition */}
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <LottieView
-              autoPlay
-              style={{
-                width: width * 0.95,
-                height: width * 0.95,
-              }}
-              source={
-                weatherForecast?.current?.condition.text &&
-                weatherForecast.current.condition.text in weatherIcons
-                  ? weatherIcons[
-                      weatherForecast.current.condition
-                        .text as keyof typeof weatherIcons
-                    ]
-                  : require("../../assets/anim/cloudy.json")
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View
+        style={
+          weatherDataCtx?.fetching
+            ? {
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: colors.background_cream,
               }
-            />
-            <Text
+            : {
+                backgroundColor: colors.background_cream,
+                flex: 1,
+                justifyContent: "space-between",
+                padding: 6,
+              }
+        }
+      >
+        {weatherDataCtx?.fetching ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <>
+            {/* locatoin details */}
+            <View
               style={{
-                fontSize: 30,
-                fontFamily: "MaiandraSD",
-                color: colors.background_dark,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginLeft: 10,
+                marginTop: 24,
               }}
             >
-              {weatherForecast?.current?.condition.text}
-            </Text>
-          </View>
+              <View style={{ flexDirection: "column" }}>
+                <Text
+                  style={{ fontFamily: "GlacialIndifference" }}
+                >{`${day}, ${date} ${month}`}</Text>
+                <Text
+                  style={{
+                    fontSize: 32,
+                    color: colors.background_dark,
+                    fontFamily: "MaiandraSD",
+                  }}
+                >
+                  {weatherForecast?.location?.name}
+                </Text>
+                <Text style={{ fontFamily: "GlacialIndifference" }}>
+                  {weatherForecast?.location?.country}
+                </Text>
+              </View>
+            </View>
 
-          {/* wether details */}
-          <View
-            style={{
-              left: 0,
-              bottom: 0,
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              padding: 10,
-            }}
-          >
-            <View style={{ flexDirection: "row" }}>
-              <Text style={styles.tempText}>
-                {weatherForecast.current?.feelslike_c}
-              </Text>
+            {/* weather condition */}
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <LottieView
+                autoPlay
+                style={{
+                  width: width * 0.95,
+                  height: width * 0.95,
+                }}
+                source={
+                  weatherForecast?.current?.condition.text &&
+                  weatherForecast.current.condition.text in weatherIcons
+                    ? weatherIcons[
+                        weatherForecast.current.condition
+                          .text as keyof typeof weatherIcons
+                      ]
+                    : require("../../assets/anim/cloudy.json")
+                }
+              />
               <Text
-                style={{ marginTop: 6, fontSize: 18, fontFamily: "MaiandraSD" }}
+                style={{
+                  fontSize: 30,
+                  fontFamily: "MaiandraSD",
+                  color: colors.background_dark,
+                }}
               >
-                °C
+                {weatherForecast?.current?.condition.text}
               </Text>
             </View>
-            {/* wind,percipation ets */}
-            <View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginVertical: 6,
-                  backgroundColor: colors.primary_gray,
-                  padding: 4,
-                  borderRadius: 8,
-                }}
-              >
-                <Image
-                  style={{
-                    width: 25,
-                    height: 25,
-                    resizeMode: "contain",
-                    marginHorizontal: 4,
-                  }}
-                  source={require("../../assets/icons/wind.png")}
-                />
+
+            {/* wether details */}
+            <View
+              style={{
+                left: 0,
+                bottom: 0,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                padding: 10,
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <Text style={styles.tempText}>
+                  {weatherForecast.current?.feelslike_c}
+                </Text>
                 <Text
-                  style={styles.otherDetailsText}
-                >{`${weatherForecast.current?.gust_kph} kph`}</Text>
+                  style={{
+                    marginTop: 6,
+                    fontSize: 18,
+                    fontFamily: "MaiandraSD",
+                  }}
+                >
+                  °C
+                </Text>
               </View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  backgroundColor: colors.primary_blue,
-                  padding: 4,
-                  borderRadius: 8,
-                }}
-              >
-                <Image
+              {/* wind,percipation ets */}
+              <View>
+                <View
                   style={{
-                    width: 25,
-                    height: 25,
-                    resizeMode: "contain",
-                    marginHorizontal: 4,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginVertical: 6,
+                    backgroundColor: colors.primary_gray,
+                    padding: 4,
+                    borderRadius: 8,
                   }}
-                  source={require("../../assets/icons/drop.png")}
-                />
-                <Text
-                  style={styles.otherDetailsText}
-                >{`${weatherForecast.current?.humidity}%`}</Text>
+                >
+                  <Image
+                    style={{
+                      width: 25,
+                      height: 25,
+                      resizeMode: "contain",
+                      marginHorizontal: 4,
+                    }}
+                    source={require("../../assets/icons/wind.png")}
+                  />
+                  <Text
+                    style={styles.otherDetailsText}
+                  >{`${weatherForecast.current?.gust_kph} kph`}</Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: colors.primary_blue,
+                    padding: 4,
+                    borderRadius: 8,
+                  }}
+                >
+                  <Image
+                    style={{
+                      width: 25,
+                      height: 25,
+                      resizeMode: "contain",
+                      marginHorizontal: 4,
+                    }}
+                    source={require("../../assets/icons/drop.png")}
+                  />
+                  <Text
+                    style={styles.otherDetailsText}
+                  >{`${weatherForecast.current?.humidity}%`}</Text>
+                </View>
               </View>
             </View>
-          </View>
-        </>
-      )}
+          </>
+        )}
+      </View>
     </ScrollView>
   );
 };
