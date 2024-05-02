58f3e7eacbe76d1e815bc4949d6bf5b918acdb71
Aswin Karki <aswin.karki44@gmail.com>
2024-05-02 12:13:42 +0545

    added splashscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,31 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import WeatherContexProvider from "../store/context/weather-context";
 
 import { useFonts } from "expo-font";
-import { ActivityIndicator } from "react-native";
+import { View, Image } from "react-native";
 
 const Layout = () => {
   let [fontsLoaded] = useFonts({
     GlacialIndifference: require("../assets/fonts/GlacialIndifference.otf"),
     MaiandraSD: require("../assets/fonts/Maiandra.otf"),
   });
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#eae5ca",
+        }}
+      >
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={require("../assets/icons/sunny.png")}
+        />
+      </View>
+    );
+  }
   return (
     <WeatherContexProvider>
       <Stack screenOptions={{ headerShown: false }}>
